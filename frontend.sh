53b5183af7e1077fefbbe4a5269afa871fb93901
Kavya Naveena <karumurikavya@gmail.com>
2023-02-25 17:17:36 +0530

    Added colors and printing statement

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,21 @@
+echo -e "\e[34mInstalling nginx \e[0m"
 yum install nginx -y
+
+echo -e "\e[35mRemoving Old Content \e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36mDownloading Frontend Content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[33mExtracting Downloaded content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[34mCopying nginx configuration file "
 cp configs/nginx.roboshop.conf /etc/nginx/deault.d/roboshop.conf
+
+echo -e "\e[35mEnabling nginx \e[0m"
 systemctl enable nginx
+
+echo -e "\e[36mStarting nginx server \e[0m"
 systemctl start nginx
